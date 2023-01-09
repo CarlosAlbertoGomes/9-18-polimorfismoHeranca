6c15e7602fb623e0c85e398795f7c351f46792cf
Carlos Gomes <gomez.3d@hotmail.com>
2023-01-08 23:57:39 -0300

    adc .java

---
+++ ControleRemoto.java
@@ -0,0 +1,17 @@
+package controleRemoto;
+public class ControleRemoto {
+    public ControleRemoto() {
+    }
+
+    public String mover(String tipoBrinquedo, boolean paraquedas) {
+        return "O brinquedo movido foi o Avião";
+    }
+
+    public String mover(String tipoBrinquedo, boolean sinalizador, boolean boia) {
+        return "O brinquedo movido foi o Barco";
+    }
+
+    public String mover(String tipoBrinquedo) {
+        return "O brinquedo movido foi o Carro";
+    }
+}
---
+++ TesteControleRemoto.java
@@ -0,0 +1,11 @@
+package controleRemoto;
+public class TesteControleRemoto {
+    public TesteControleRemoto() {
+    }
+    public static void main(String[] args) {
+        ControleRemoto controleRemoto = new ControleRemoto();
+        System.out.println("Chamando carro" + controleRemoto.mover("Carro"));
+        System.out.println("Chamando avião" + controleRemoto.mover("Avião", true));
+        System.out.println("Chamando avião" + controleRemoto.mover("Barco", true, true));
+    }
+}
